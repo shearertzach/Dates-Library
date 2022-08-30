ad3f48aee6504c18f2cb08937e0bf872d7d28f21
shearertzach <shearertzach@gmail.com>
2022-08-30 13:24:24 -0500

    Added format function

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ const months = ['January', 'Feburary', 'March', 'April', 'May', 'June', 'July', 
 const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
 class D {
-	constructor(...args) {
-		this._date = new Date(...args)
-	}
+  constructor(...args) {
+    this._date = new Date(...args)
+  }
 
   get year() {
     return this._date.getFullYear()
@@ -35,5 +35,65 @@ class D {
   }
   get secs() {
     return this._date.getSeconds()
+  }
+
+  formatDate(format) {
+    if (typeof (format) != "string") return
+    const formats = format.split('')
+    let formattedDate = ""
+
+    for (let i = 0; i < formats.length; i++) {
+      switch (formats[i]) {
+        case "Y":
+          formattedDate = formattedDate + this.year
+          break
+        case "y":
+          formattedDate = formattedDate + this.yr
+          break
+        case "M":
+          formattedDate = formattedDate + this.month
+          break
+        case "m":
+          formattedDate = formattedDate + this.mon
+          break
+        case "D":
+          formattedDate = formattedDate + this.date
+          break
+        case "d":
+          const date = this.date.toString()
+          if (date.length > 1) formattedDate = formattedDate + date
+          else formattedDate = formattedDate + `0${date}`
+          break
+        case "H":
+          const hrs = (this.hours % 12).toString()
+          if (hrs.length > 1) formattedDate = formattedDate + hrs
+          else formattedDate = formattedDate + `0${hrs}`
+          break
+        case "h":
+          formattedDate = formattedDate + this.hours
+          break
+        case "I":
+          const mins = this.mins.toString()
+          if (mins.length > 1) formattedDate = formattedDate + mins
+          else formattedDate = formattedDate + `0${mins}`
+          break
+        case "i":
+          formattedDate = formattedDate + this.mins
+          break
+        case "S":
+          const secs = this.secs.toString()
+          console.log(secs.length)
+          if (secs.length > 1) formattedDate = formattedDate + secs
+          else formattedDate = formattedDate + `0${secs}`
+          break
+        case "s":
+          formattedDate = formattedDate + this.secs
+          break
+        default:
+          formattedDate = formattedDate + formats[i]
+          break
+      }
+    }
+    return formattedDate
   }
 }
